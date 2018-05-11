c0066c92f58648f6beae195f9a689e1777f22ac4
Kenneth Morton <kennethmorton@gmail.com>
2018-05-11 10:35:47 -0400

    separate gamma and xray answer format api endpoints

--- ranker/api/server.py
+++ ranker/api/server.py
@@ -54,9 +54,53 @@ class AnswerQuestion(Resource):
         answer = answer_question.apply(args=[question]).result
         if isinstance(answer, BaseException):
             return "No answers", 204
-        return answer.toStandard(), 200
+        return answer.toJSON(), 200
 
 api.add_resource(AnswerQuestion, '/')
+
+class AnswerQuestionStandard(Resource):
+    def post(self):
+        """
+        Get answers to a question in standard format
+        ---
+        parameters:
+          - in: body
+            name: question
+            description: The machine-readable question graph.
+            schema:
+                $ref: '#/definitions/Question'
+            required: true
+        responses:
+            200:
+                description: Answer
+                schema:
+                    type: object
+                    required:
+                      - thingsandstuff
+                    properties:
+                        thingsandstuff:
+                            type: string
+                            description: all the things and stuff
+        """
+        # replace `parameters` with this when OAS 3.0 is fully supported by Swagger UI
+        # https://github.com/swagger-api/swagger-ui/issues/3641
+        """
+        requestBody:
+            description: The machine-readable question graph.
+            required: true
+            content:
+                application/json:
+                    schema:
+                        $ref: '#/definitions/Question'
+        """
+        question = Question(request.json)
+        logger.debug(question.cypher_match_string())
+        answer = answer_question.apply(args=[question]).result
+        if isinstance(answer, BaseException):
+            return "No answers", 204
+        return answer.toStandard(), 200
+
+api.add_resource(AnswerQuestionStandard, '/standard/')
 
 class QuestionSubgraph(Resource):
     def post(self):
