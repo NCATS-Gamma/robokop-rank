cc8fcbd0f93ac4cda2ce35ca17f45d3a4245d45e
patrickkwang <patrick@covar.com>
2018-09-10 20:31:02 -0400

    cleaning up outdated api endpoints

--- ranker/api/server.py
+++ ranker/api/server.py
@@ -21,76 +21,6 @@ from ranker.knowledgegraph import KnowledgeGraph
 
 logger = logging.getLogger("ranker")
 
-class QueryTemplate(Resource):
-    def post(self):
-        """
-        Query
-        ---
-        tags: [query]
-        summary: "Query RTX using a predefined question type"
-        description: ""
-        operationId: "query"
-        consumes:
-          - "application/json"
-        produces:
-          - "application/json"
-        parameters:
-          - in: "body"
-            name: "body"
-            description: "Query information to be submitted"
-            required: true
-            schema:
-                $ref: "#/definitions/Query"
-        responses:
-            200:
-                description: "successful operation"
-                schema:
-                    $ref: "#/definitions/Response"
-            400:
-                description: "Invalid status value"
-        """
-        logger.debug(f"Received request {request.json}.")
-        if not request.json['query_type_id'] == 'Q3':
-            return f"I don't know what a '{request.json['query_type_id']} is.", 200
-        drug_id = request.json['terms']['chemical_substance']
-        q_json = {
-            "edges": [
-                {
-                    "end": 1,
-                    "start": 0
-                }
-            ],
-            "nodes": [
-                {
-                    "id": 0,
-                    "identifiers": [
-                        drug_id
-                    ],
-                    "type": "chemical_substance"
-                },
-                {
-                    "id": 1,
-                    "type": "gene"
-                }
-            ]
-        }
-        question = Question(q_json)
-        answerset = answer_question(question)
-        if isinstance(answerset, BaseException):
-            response = {
-                'datetime': datetime.now().isoformat(),
-                'id': '',
-                'message': f"Found 0 answers.",
-                'response_code': 'OK'
-            }
-        else:
-            response = answerset.toStandard()
-            response.update(request.json)
-        logger.debug(f"Prepared response {response}.")
-        return response, 200
-
-api.add_resource(QueryTemplate, '/query')
-
 class AnswerQuestionNow(Resource):
     def post(self):
         """
@@ -175,51 +105,6 @@ class AnswerQuestion(Resource):
 
 api.add_resource(AnswerQuestion, '/')
 
-class AnswerQuestionStandard(Resource):
-    def post(self):
-        """
-        Get answers to a question in standard format
-        ---
-        tags: [answer]
-        parameters:
-          - in: body
-            name: question
-            description: The machine-readable question graph.
-            schema:
-                $ref: '#/definitions/Question'
-            required: true
-        responses:
-            200:
-                description: Answer
-                schema:
-                    type: object
-                    required:
-                      - thingsandstuff
-                    properties:
-                        thingsandstuff:
-                            type: string
-                            description: all the things and stuff
-        """
-        # replace `parameters` with this when OAS 3.0 is fully supported by Swagger UI
-        # https://github.com/swagger-api/swagger-ui/issues/3641
-        """
-        requestBody:
-            description: The machine-readable question graph.
-            required: true
-            content:
-                application/json:
-                    schema:
-                        $ref: '#/definitions/Question'
-        """
-        question = Question(request.json)
-        logger.debug(question.cypher_match_string())
-        answer = answer_question(question)
-        if isinstance(answer, BaseException):
-            return "No answers", 204
-        return answer.toStandard(), 200
-
-api.add_resource(AnswerQuestionStandard, '/standard/')
-
 class QuestionSubgraph(Resource):
     def post(self):
         """
@@ -260,63 +145,6 @@ class QuestionSubgraph(Resource):
         return subgraph, 200
 
 api.add_resource(QuestionSubgraph, '/subgraph')
-
-class IDMap(Resource):
-    def post(self, concept):
-        """
-        Get id map
-        ---
-        tags: [util]
-        parameters:
-          - in: path
-            name: concept
-            description: Biolink concept
-            type: string
-            required: true
-        responses:
-            200:
-                description: Knowledge subgraph
-                schema:
-                    type: object
-        """
-
-        database = KnowledgeGraph()
-        id_map = database.get_map_for_type(concept)
-
-        return id_map, 200
-
-api.add_resource(IDMap, '/id_map/<concept>')
-
-class MapID(Resource):
-    def post(self, concept, identifier):
-        """
-        Get canonical id, IF AVAILABLE IN NEO4J CACHE
-        ---
-        tags: [util]
-        parameters:
-          - in: path
-            name: concept
-            description: Biolink concept
-            type: string
-            required: true
-          - in: path
-            name: identifier
-            description: curie
-            type: string
-            required: true
-        responses:
-            200:
-                description: Knowledge subgraph
-                schema:
-                    type: string
-        """
-
-        database = KnowledgeGraph()
-        id_map = database.get_map_for_type(concept)
-
-        return id_map[identifier], 200
-
-api.add_resource(MapID, '/canonicalize/<concept>/<identifier>')
 
 class Tasks(Resource):
     def get(self):
