f9ddf73df4b47d87b4a73e192c127c9ceece53de
patrickkwang <patrick@covar.com>
2018-10-15 20:43:05 -0400

    edge ids are unmodified strings now: modify the tests

--- ranker/test/test_ranker.py
+++ ranker/test/test_ranker.py
@@ -6,19 +6,19 @@ def test_hi():
     assert True
 
 def test_one_hop():
-    q = { 'nodes': [ {"id": 0, "type": "disease", "curie": "MONDO:0005148"},
-                     {"id": 1, "type": "chemical_substance" }],
-          'edges': [ {'source_id': 1, 'target_id': 0, 'id':'a'} ]
+    q = { 'nodes': [ {"id": 'n0', "type": "disease", "curie": "MONDO:0005148"},
+                     {"id": 'n1', "type": "chemical_substance" }],
+          'edges': [ {'source_id': 'n1', 'target_id': 'n0', 'id':'ea'} ]
           }
     g = {
          'nodes': [ {"id": "MONDO:0005148", "type": "disease"},
                     {"id": "CHEBI:17234", "type": "chemical_substance"}],
-         'edges': [ {'id': 1, 'target_id': 'MONDO:0005148', 'source_id': 'CHEBI:17234', 'type':'contributes_to'},
+         'edges': [ {'id': '1', 'target_id': 'MONDO:0005148', 'source_id': 'CHEBI:17234', 'type':'contributes_to'},
                     {'id': 'xyz', 'target_id': 'MONDO:0005148', 'source_id': 'CHEBI:17234', 'type':'literature_co-occurrence','publications':range(10)}]
     }
     sg = {
         'nodes': {'n0':'MONDO:0005148', 'n1':'CHEBI:17234'},
-        'edges': {'ea': [1], 's0': 'xyz'}
+        'edges': {'ea': ['1'], 's0': 'xyz'}
     }
     ranker = Ranker(question = q, graph = g)
     #Normally weights calculated in code, but here generating by fiat
@@ -28,13 +28,13 @@ def test_one_hop():
     v = ranker.subgraph_statistic(sg,metric_type='volt')
 
 def test_three_hop():
-    q = { 'nodes': [ {"id": 0, "type": "disease", "curie": "MONDO:0005148"},
-                     {"id": 1, "type": "chemical_substance" },
-                     {"id": 2, "type": "gene" },
-                     {"id": 3, "type": "chemical_substance" }],
-          'edges': [ {'source_id': 1, 'target_id': 0, 'id':'a'},
-                     {'source_id': 2, 'target_id': 1, 'id':'b'},
-                     {'source_id': 3, 'target_id': 2, 'id':'c'} ]
+    q = { 'nodes': [ {"id": 'n0', "type": "disease", "curie": "MONDO:0005148"},
+                     {"id": 'n1', "type": "chemical_substance" },
+                     {"id": 'n2', "type": "gene" },
+                     {"id": 'n3', "type": "chemical_substance" }],
+          'edges': [ {'source_id': 'n1', 'target_id': 'n0', 'id':'ea'},
+                     {'source_id': 'n2', 'target_id': 'n1', 'id':'eb'},
+                     {'source_id': 'n3', 'target_id': 'n2', 'id':'ec'} ]
           }
     g = {
          'nodes': [ {"id": "MONDO:0005148", "type": "disease"},
@@ -42,20 +42,20 @@ def test_three_hop():
                     {"id": "HGNC:601", "type": "gene"},
                     {"id": "CHEBI:123", "type": "chemical_substance"}
                     ],
-         'edges': [ {'id': 1, 'target_id': 'MONDO:0005148', 'source_id': 'CHEBI:17234', 'type':'contributes_to'},
-                    {'id': 2, 'source_id': 'HGNC:601', 'target_id': 'CHEBI:17234', 'type':'increases_response_to'},
-                    {'id': 3, 'target_id': 'HGNC:601', 'source_id': 'CHEBI:123', 'type':'increases_secretion_of'},
+         'edges': [ {'id': '1', 'target_id': 'MONDO:0005148', 'source_id': 'CHEBI:17234', 'type':'contributes_to'},
+                    {'id': '2', 'source_id': 'HGNC:601', 'target_id': 'CHEBI:17234', 'type':'increases_response_to'},
+                    {'id': '3', 'target_id': 'HGNC:601', 'source_id': 'CHEBI:123', 'type':'increases_secretion_of'},
                     {'id': '1to2', 'target_id': 'MONDO:0005148', 'source_id': 'CHEBI:17234', 'type':'literature_co-occurrence','publications':range(10)},
                     {'id': '1to3', 'target_id': 'MONDO:0005148', 'source_id': 'HGNC:601', 'type':'literature_co-occurrence','publications':range(10)},
                     {'id': '1to4', 'target_id': 'MONDO:0005148', 'source_id': 'CHEBI:123', 'type':'literature_co-occurrence','publications':range(10)},
                     {'id': '2to3', 'target_id': 'HGNC:601', 'source_id': 'CHEBI:17234', 'type':'literature_co-occurrence','publications':range(10)},
                     {'id': '2to4', 'target_id': 'CHEBI:123', 'source_id': 'CHEBI:17234', 'type':'literature_co-occurrence','publications':range(10)},
                     {'id': '3to4', 'target_id': 'HGNC:601', 'source_id': 'CHEBI:123', 'type':'literature_co-occurrence','publications':range(10)},
-                    {'id': 4, 'target_id': 'MONDO:0005148', 'source_id': 'CHEBI:17234', 'type':'contributes_to'}]
+                    {'id': '4', 'target_id': 'MONDO:0005148', 'source_id': 'CHEBI:17234', 'type':'contributes_to'}]
     }
     sg = {
         'nodes': {'n0':'MONDO:0005148', 'n1':'CHEBI:17234', 'n2': 'HGNC:601', 'n3': 'CHEBI:123'},
-        'edges': {'ea': [1,4], 'eb': [2], 'ec': [3],  's0': '1to2', 's1':'1to3', 's2':'1to4', 's3': '2to3', 's4': '2to4', 's5': '3to4'}
+        'edges': {'ea': ['1','4'], 'eb': ['2'], 'ec': ['3'],  's0': '1to2', 's1':'1to3', 's2':'1to4', 's3': '2to3', 's4': '2to4', 's5': '3to4'}
     }
     ranker = Ranker(question = q, graph = g)
     #Normally weights calculated in code, but here generating by fiat
